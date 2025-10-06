69642528f81dd555d0442994055a5a253a1afc91
“ElisaGalasso“ <galassoelisa72@gmail.com>
2025-10-06 20:46:08 +0200

    style with bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -21,18 +21,29 @@ function App() {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input type='text' placeholder='Inserisci un prodotto' value={listProducts}
-          onChange={(e) => setListProducts(e.target.value)}></input>
-        <button type='submit'>Add to List</button>
-      </form>
-      <ul>
-        {newListProducts.map((product, i) =>
-          <li key={i}>
-            {product}
-          </li>
-        )}
-      </ul>
+      <div className="container mt-5 d-flex flex-column align-items-center">
+        <div className="card">
+          <div className="card-body">
+            <form onSubmit={handleSubmit}>
+              <input className='me-2 mb-3'
+                type='text'
+                placeholder='Inserisci un prodotto'
+                value={listProducts}
+                onChange={(e) => setListProducts(e.target.value)} required>
+              </input>
+              <button className='btn btn-primary' type='submit'>Add to List</button>
+            </form>
+            <ul className='list-group'>
+              {newListProducts.map((product, i) =>
+                <li key={i} className='list-group-item'>
+                  {product}
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
